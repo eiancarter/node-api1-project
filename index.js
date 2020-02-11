e4326a352514a96e6c3e58652c7dd67e30fdc665
Eian Carter <eian@bricleir.com>
2020-02-11 12:04:26 -0800

    added catch

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ server.get('/api/users/:id', (req, res) => {
         res.status(200).json(user);
     }).catch(err=> {
         console.log(err)
-        res.status(500).json({ errorMessage: 'The user information could not be retrieved.'})
+        if (!user.id) {
+            res.status(404).json({ errorMessage: 'The user with the specified ID does not exist.' })
+        } else {
+            res.status(500).json({ errorMessage: 'The user information could not be retrieved.'}) 
+        }
     })
 })
 //post a new user
@@ -35,7 +39,11 @@ server.post('/api/users', (req, res) => {
         res.status(201).json(user)
     }).catch(error => {
         console.log(error)
-        res.status(500).json({ errorMessage: 'There was an error while saving the user to the database'})
+        if (!userInfo.name || !userInfo.bio) {
+            res.status(400).json({ errorMessage: 'Please provide name and bio for the user.'})
+        } else {
+        res.status(500).json({ errorMessage: 'There was an error while saving the user to the database' })
+        }
     })
 })
 //delete a user 
@@ -50,18 +58,16 @@ server.delete('/api/users/:id', (req, res) => {
 })
 //edits a user
 server.put('/api/users/:id', (req, res) => {
-
-    const updateUser = req.body
-
-    db.update(updateUser.params.id).then( user => {
+    const editUser = req.body
+    db.update(req.params.id, editUser).then( user => {
+        res.status(200).json(user);
+    }).catch(err => {
+        console.log(err);
         if (!name || !bio) {
             res.status(400).json({ errorMessage: 'Please provide name and bio for the user.'})
         } else {
-            res.status(200).json(user);
-        }
-    }).catch(err => {
-        console.log(err);
         res.status(500).json({ errorMessage: 'The user information could not be modified.' })
+        }
     })
 })
 
